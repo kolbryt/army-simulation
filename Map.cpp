4ebeadda4f981123f2bf55e7e6927f9050c48d3e
Kajetan Olbryt <k.olbryt@gmail.com>
2026-03-06 23:29:46 +0100

    Refactor Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -6,18 +6,18 @@ Map::Map() {
 }
 
 void Map::setUnitsCoordinates() {
-    for (int row{0}; row < 8; ++row) {
-        for (int column{0}; column < 8; ++column) {
+    for (unsigned int row{0}; row < 8; ++row) {
+        for (unsigned int column{0}; column < 8; ++column) {
             units[row][column].setCoordinates(row, column);
         }
     }
 }
 
 void Map::show() {
-    for (int row{0}; row < 8; ++row) {
-        for (int column{0}; column < 8; ++column) {
+    for (unsigned int row{0}; row < 8; ++row) {
+        for (unsigned int column{0}; column < 8; ++column) {
             std::cout << units[row][column].getVisualization() << ' ';
         }
-        std::cout << std::endl;
+        std::cout << '\n';
     }
 }
